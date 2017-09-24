608df6ee7ed9dded6c325f1522ed1f33ba3847bf
Tibor Papp <mortens.4444@gmail.com>
2017-09-25 00:10:07 +0200

    Serial ActionExecutor has been added.

--- Control/Drone-Drain -untested/DroneDrain.ino
+++ Control/Drone-Drain -untested/DroneDrain.ino
@@ -1,6 +1,7 @@
 #include "MovingValueManager.h"
 #include "SerialController.h"
 #include "MoveController.h"
+#include "ActionExecutor.h"
 
 //this program will put out a PPM signal
 
@@ -18,12 +19,14 @@
 MovingValueManager *movingValueManager;
 MoveController *moveController;
 SerialController* serialController;
+ActionExecutor* actionExecutor;
 
 void setup()
 {
   serialController = new SerialController(9600);
   movingValueManager = new MovingValueManager();
   moveController = new MoveController(movingValueManager, serialController);
+  actionExecutor = new ActionExecutor(moveController);
 
   pinMode(sigPin, OUTPUT);
   digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
@@ -40,22 +43,75 @@ void setup()
 
   movingValueManager->SetMoveValue(MoveController::DefaultMoveValue);
   movingValueManager->SetMoveInterval(MoveController::DefaultInterval);
-  movingValueManager->SetBrakeMoveValue(MoveController::DefaultBrakeMoveValue);
-  movingValueManager->SetBrakeMoveInterval(MoveController::DefaultBrakeInterval);
   movingValueManager->SetSmooth(MoveController::DefaultSmooth);
   movingValueManager->SetSmoothInterval(MoveController::DefaultSmoothInterval);
+  movingValueManager->SetBrakeMoveValue(MoveController::DefaultBrakeMoveValue);
+  movingValueManager->SetBrakeMoveInterval(MoveController::DefaultBrakeInterval);
+  movingValueManager->SetBrakeSmooth(MoveController::DefaultBrakeSmooth);
+  movingValueManager->SetBrakeSmoothInterval(MoveController::DefaultBrakeSmoothInterval);
 }
 
+const char separator = ';';
+String command, part;
+
 void loop()
-{ 
-//  moveController->MoveUp();
-//  moveController->MoveForward();
-//  moveController->MoveBackward();
-//  moveController->MoveLeft();
-//  moveController->MoveRight();
-//  moveController->TurnLeft();
-//  moveController->TurnRight();
-//  moveController->MoveDown();
+{
+  command = serialController->ReadLine();
+  if (command != "")
+  {
+    if (command.indexOf(separator) > -1)
+    {
+      int startIndex = 0, endIndex, resultIndex = 0;
+      do
+      {
+        endIndex = command.indexOf(separator, startIndex + 1);
+        part = endIndex == -1 ? command.substring(startIndex, command.length()) : command.substring(startIndex, endIndex);
+
+        switch (resultIndex++)
+        {
+          case 0:
+            serialController->WriteLine("Set move value to: " + part);
+            movingValueManager->SetMoveValue(part.toInt());
+            break;
+          case 1:
+            serialController->WriteLine("Set move interval to: " + part);
+            movingValueManager->SetMoveInterval(part.toInt());
+            break;
+          case 2:
+            serialController->WriteLine("Set smooth value to: " + part);
+            movingValueManager->SetSmooth(part.toInt());
+            break;
+          case 3:
+            serialController->WriteLine("Set smooth interval to: " + part);
+            movingValueManager->SetSmoothInterval(part.toInt());
+            break;
+          case 4:
+            serialController->WriteLine("Set brake value to: " + part);
+            movingValueManager->SetBrakeMoveValue(part.toInt());
+            break;
+          case 5:
+            serialController->WriteLine("Set brake interval to: " + part);
+            movingValueManager->SetBrakeMoveInterval(part.toInt());
+            break;
+          case 6:
+            serialController->WriteLine("Set brake smooth value to: " + part);
+            movingValueManager->SetBrakeSmooth(part.toInt());
+            break;
+          case 7:
+            serialController->WriteLine("Set brake smooth interval to: " + part);
+            movingValueManager->SetBrakeSmoothInterval(part.toInt());
+            break;
+        }
+        startIndex = endIndex + 1;
+      }
+      while (endIndex != -1);
+    }
+    else
+    {
+      serialController->WriteLine("Received command: " + command);
+      actionExecutor->ExecuteCommand(command);
+    }
+  }
 }
 
 ISR(TIMER1_COMPA_vect)
